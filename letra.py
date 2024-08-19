e81c5223f0f9aa6bbe628056de4c34d22b8fa7b9
Blaj3n <benceulrich@gmail.com>
2024-08-19 12:36:29 +0200

    dobások összeadása, ellenőrzése

--- letra.py
+++ letra.py
@@ -1,5 +1,16 @@
-dobasok = [3, 1, 1, 2, 1, 5, 5, 4, 4, 4, 1, 2, 3, 6, 4, 6, 1, 4]
+dobasok = [3, 1, 1, 2, 1, 5, 5, 4, 4, 4, 1, 2, 3, 6, 4, 6, 1, 4, 5, 4]
 
-for elem in dobasok:
-    print(f"{elem}")
+
 print("2. feladat")
+osszeg = 0
+for szam in dobasok:
+    osszeg += szam
+    if osszeg % 10 == 0:
+        osszeg -= 3
+    elif osszeg > 45:
+        print(osszeg)
+        break
+    print(osszeg, end=" ")
+print("\n4. feladat")
+if osszeg >= 45:
+    print("A játékot befejezte")
